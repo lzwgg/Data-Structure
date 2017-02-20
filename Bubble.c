cb2972361997eb6183e684079a12e0765dc62f73
lzwgg <lzwiread@gmail.com>
2017-02-20 10:04:21 +0800

    Update Bubble.c

--- Bubble.c
+++ Bubble.c
@@ -5,9 +5,10 @@
 int main(void)
 {
   int a[MaxSize] = {6,4,2,1,5,9,3};
-  int i, j, tmp;
+  int i, j, tmp, sortedFlag;
   for (i = 0; i < MaxSize; i++)
   {
+    sortedFlag = 0; // added to judge the array whether sorted or not before loop ends
     for (j = 0; j < MaxSize - 1; j++) // because we campare a[j+1] with a[j], so j < MaxSize - 1
     {
       if (a[j+1] < a[j])
@@ -15,7 +16,10 @@ int main(void)
         tmp = a[j];
         a[j] = a[j+1];
         a[j+1] = tmp;
+        sortedFlag = 1;
       }
     }
+    if (!sortedFlag)  break;
   }
+  return 0;
 }
