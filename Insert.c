eec9b91b2682f8956a09637d1a6fd305ba150811
lzwgg <lzwiread@gmail.com>
2017-02-20 10:48:04 +0800

    Create Insert.c

---
+++ Insert.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MaxSize 7
+
+int main(void)
+{
+  int a[MaxSize] = {6,4,2,1,5,9,3};
+  int i, j, tmp;
+  for (i = 1; i < MaxSize; i++)
+  {
+    tmp = a[i];
+    j = i - 1;
+    for (j >= 0 && a[j] < tmp)
+    {
+      a[j+1] = a[j];
+    }
+    a[j+1] = tmp;
+  }
+  return 0;
+}
