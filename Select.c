272fbb338eb53f59e5c37c14b4403be6314e0ac9
lzwgg <lzwiread@gmail.com>
2017-02-20 10:39:02 +0800

    Update Select.c

--- Select.c
+++ Select.c
@@ -5,17 +5,20 @@
 int main(void)
 {
   int a[MaxSize] = {6,4,2,1,5,9,3};
-  int i, j, tmp;
+  int i, j, k, tmp;
   for (i = 0; i < MaxSize; i++)
   {
+    k = i;
     for (j = i + 1; j < MaxSize; j++)
     {
-      if (a[j] < a[i])
-      {
+      if (a[j] < a[k])
+        k = j;
+    }
+    if (k != i)
+    {
         tmp = a[j];
         a[j] = a[i];
         a[i] = tmp;
-      }
     }
   }
   return 0;
