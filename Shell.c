42a1ea72119c75872aa42f617959e485a3cf4e84
lzwgg <lzwiread@gmail.com>
2017-02-20 11:17:08 +0800

    Create Shell.c

---
+++ Shell.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MaxSize 7
+
+int main(void)
+{
+  int a[MaxSize] = {6,4,2,1,5,9,3};
+  int i, j, gap;
+  gap = MaxSize / 2;
+  while (gap > 0)
+  {
+    for (i = gap; i < MaxSize; i++)
+    {
+      tmp = a[i];
+      j = i - gap;
+      while (j >= 0 && tmp < a[j])
+      {
+        a[j+gap] = a[j];
+        j -= gap;
+      }
+      a[j+gap] = tmp;
+    }
+    gap /= 2;
+  }
+  return 0;
+}
